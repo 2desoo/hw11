d257add84401d6c70d142aed4d37c1a98e95666c
desoo <marcus60.2001@gmail.com>
2023-11-29 22:28:11 +0300

    Changed homework

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void info(String test) {
+        System.out.println(test);
+    }
+
     public static void printYearLeap(int year) {
+        info("Task #1");
         boolean leapYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
         if (leapYear) {
             System.out.println(year + (" год является високосным."));
@@ -9,39 +20,42 @@ public class Main {
         }
     }
 
+
     public static void checkDevice(int clientOs, int clientDeviceYear) {
+        info("Task #2");
         int iOs = 0;
         int android = 1;
         if (clientOs == iOs && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-        } else if (clientOs == iOs && clientDeviceYear >= 2015) {
+        } else if (clientOs == iOs && clientDeviceYear > 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
         } else if (clientOs == android && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-        } else if (clientOs == android && clientDeviceYear >= 2015) {
+        } else if (clientOs == android && clientDeviceYear > 2015) {
             System.out.println("Установите версию приложения для Android по ссылке.");
         }
     }
 
-    public static void checkDeliveryDistance(int deliveryDistance, int day) {
-        if (deliveryDistance <= 20) {
-            day++;
-            System.out.println("Доставка займёт " + day + " день.");
-        } else if (deliveryDistance > 21 && deliveryDistance <= 60) {
-            day += 2;
-            System.out.println("Доставка займёт " + day + " дня.");
-        } else if (deliveryDistance > 61 && deliveryDistance <= 100) {
-            day += 3;
-            System.out.println("Доставка займёт " + day + " дня.");
+    public static int checkDeliveryDistance(int a) {
+        info("Task #3");
+        int day = 1;
+        if (a <= 20) {
+            return day;
+        } else if (a > 21 && a <= 60) {
+            return day + 1;
+        } else if (a > 61 && a <= 100) {
+            return day + 2;
         } else {
-            System.out.println("Доставки нет!");
+            return -1;
         }
     }
 
-    public static void main(String[] args) {
-        task1();
-        task2();
-        task3();
+    public static void time(int days) {
+        if (days > 0) {
+            info("Доставка займёт " + days + " дня.");
+        } else {
+            info("Доставки нет!");
+        }
     }
 
     private static void task1() {
@@ -53,6 +67,8 @@ public class Main {
     }
 
     private static void task3() {
-        checkDeliveryDistance(101, 0);
+        int deliveryDistance = 95;
+        int deliveryTime = checkDeliveryDistance(deliveryDistance);
+        time(deliveryTime);
     }
 }
